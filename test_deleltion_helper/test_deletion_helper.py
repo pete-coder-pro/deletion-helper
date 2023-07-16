2a243622c251a8179a56fa2614778b9dec017106
Peter Trost <trost@intervall.io>
2023-07-16 13:27:14 +0200

    Restructure project

--- test_deleltion_helper/test_deletion_helper.py
+++ test_deleltion_helper/test_deletion_helper.py
@@ -5,7 +5,7 @@ import re
 from typing import List
 import pytest
 from pytest_mock import MockerFixture
-from delete_images import delete_images
+from deletion_helper.deletion_helper import delete_images
 
 
 @pytest.fixture
@@ -14,7 +14,7 @@ def setup_files(tmp_path: Path):
     Create 20 files with random extensions in a temporary directory.
 
     Args:
-        tmp_path (Path): A temporary directory.
+        tmp_path (Path)f: A temporary directory.
     """
     extensions = ["png", "jpg", "txt", "doc", "mp3"]
     files = []
@@ -27,9 +27,9 @@ def setup_files(tmp_path: Path):
     return files
 
 
-def test_delete_images(setup_files: List[str], mocker: MockerFixture):
+def test_deletion_helper(setup_files: List[str], mocker: MockerFixture):
     """
-    Test that delete_images deletes all .png and .jpg files in a folder
+    Test that deletion_helper deletes all .png and .jpg files in a folder
     and its subfolders.
 
     Args:
@@ -49,8 +49,8 @@ def test_delete_images(setup_files: List[str], mocker: MockerFixture):
         if os.path.basename(f).endswith(tuple(pattern.split("/")[-1].split(".")[1:]))
     ]
 
-    delete_images_calls = []
-    remove_mock.side_effect = lambda x: delete_images_calls.append(x)
+    deletion_helper_calls = []
+    remove_mock.side_effect = lambda x: deletion_helper_calls.append(x)
 
     expected_deletion_count = len(
         [f for f in setup_files if f.endswith(".png") or f.endswith(".jpg")]
@@ -59,7 +59,7 @@ def test_delete_images(setup_files: List[str], mocker: MockerFixture):
     folder = Path("/any/folder")
     delete_images(folder)
 
-    assert len(delete_images_calls) == expected_deletion_count
+    assert len(deletion_helper_calls) == expected_deletion_count
 
 
 def test_non_existing_folder(mocker):
