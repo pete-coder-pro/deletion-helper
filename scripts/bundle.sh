d49bb55a4a62c16a6ae3df1088a29dc358820dba
Peter Trost <trost@intervall.io>
2023-07-16 13:28:59 +0200

    Fix typo

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # This script is used to bundle the application into a single file
-poetry run pyinstaller --onefile --name="deletion-helper" deletion_helper/deletion_helper.py
+poetry run pyinstaller --onefile --name="deletion_helper" deletion_helper/deletion_helper.py
